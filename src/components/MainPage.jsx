202b503240d77c225da32b59830f485b7e435af3
etiwitty <atiyeh.keshavarz@gmail.com>
2020-10-21 14:35:38 -0700

    fixed a bug in package json related to this ticket "https://github.com/facebook/create-react-app/issues/9255"

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(111);
+    }
+
+    render() {
+        return (
+            <h2>Hello World!</h2>
+        )
+    }
+}
+
+export default MainPage;
