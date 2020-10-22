28e34c518889d54da94c21b6ac234fb4f768db70
etiwitty <atiyeh.keshavarz@gmail.com>
2020-10-21 17:18:42 -0700

    resolved the CORS issue

--- src/components/ToppingCombo.jsx
+++ src/components/ToppingCombo.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+
 
 class ToppingCombo extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            results: []
+        }
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    
     handleSubmit(event) {
-      event.preventDefault();
-      console.log('The button was clicked.');
+        event.preventDefault();
+    //   console.log('The button was clicked.');
+        const proxyurl = "https://cors-anywhere.herokuapp.com/"; // by https://stackoverflow.com/a/43881141
+        const url = "https://www.olo.com/pizzas.json";
+        
+        axios.get(`${proxyurl}${url}`)
+            .then((res) => {
+                console.log(res);
+                this.setState({ results: res.data});
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     render() {
